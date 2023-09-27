ec7940b47fe90e6cca1844f04231004ec7dea259
maosongran <maosongran@gmail.com>
2023-09-27 15:10:30 +0800

    fix top nav

--- js/script.js
+++ js/script.js
@@ -1,67 +1,95 @@
- var pubs = $("#publications > table")
- console.log($("#publications").find("font").length)
- var total_num = 0
+var pubs = $("#publications > table")
+console.log($("#publications").find("font").length)
+var total_num = 0
 
- var years = new Array();
- var paper_nums = new Array()
- pubs.each(function(index, node) {
-    var paper_num_by_year = $(node).find("tr").length;
-    var year = $("#publications").find("font").eq(index).text();
-    $("#publications").find("font").eq(index).children().text(year + "(" + paper_num_by_year + ")");
-    total_num += paper_num_by_year;
+var years = new Array();
+var paper_nums = new Array()
+pubs.each(function (index, node) {
+   var paper_num_by_year = $(node).find("tr").length;
+   var year = $("#publications").find("font").eq(index).text();
+   $("#publications").find("font").eq(index).children().text(year + "(" + paper_num_by_year + ")");
+   total_num += paper_num_by_year;
 
-    years.push(year);
-    paper_nums.push(paper_num_by_year);
- });
-
-
- $("#publications > h2").text("Publication(" + total_num + ")");
+   years.push(year);
+   paper_nums.push(paper_num_by_year);
+});
 
 
+
+
+years = years.reverse()
+paper_nums = paper_nums.reverse()
 // for statistics
 
-const ctx = document.getElementById('myChart');
-ctx.height = 400;
-const labels =years;  // 设置 X 轴上对应的标签
-const data = {
-  labels: labels,
-  
-  datasets: [{
-      label: 'Paper: ',
-      data:paper_nums,
-      fill: true,
-      pointStyle: 'triangle',
-      borderWidth: 3,
-      pointRadius:3,    //设置圆点半径
-      pointHoverRadius: 10, //设置鼠标移动上去后圆点半径
-      tension: 0.1,
-      backgroundColor: [
-         'rgba(158, 221, 255, 0.5)',
-         // 'rgba(54, 162, 235, 0.2)',
-         // 'rgba(255, 206, 86, 0.2)',
-         // 'rgba(75, 192, 192, 0.2)',
-         // 'rgba(153, 102, 255, 0.2)',
-         // 'rgba(255, 159, 64, 0.2)'
-      ],
-      borderColor: [
+var statistics_appear = false
+statistics
+var $win = $(window);
+var itemOffsetTop = $("#statistics").offset().top;
+var itemOuterHeight = $("#statistics").outerHeight();
+var winHeight = $win.height();
+$win.scroll(function () {
+   var winScrollTop = $win.scrollTop();
+   if (!(winScrollTop > itemOffsetTop + itemOuterHeight) && !(winScrollTop < itemOffsetTop - winHeight) ) {
+      if (!statistics_appear) {
+         plotChart(years, paper_nums);
+         statistics_appear = true;
+      }
+      
+   } else {
+      statistics_appear = false;
+   }
+})
+
+
+function plotChart(years, paper_nums) {
+   $("#myChart").remove();
+   $("#statistics").append('<canvas id="myChart" width="400" height="300"></canvas>');
+   const ctx = document.getElementById('myChart');
+   ctx.height = 400;
+   const labels = years;  // 设置 X 轴上对应的标签
+   const data = {
+      labels: labels,
+
+      datasets: [{
+         label: 'Paper: ',
+         data: paper_nums,
+         fill: true,
+         pointStyle: 'triangle',
+         borderWidth: 2,
+         pointRadius: 3,    //设置圆点半径
+         pointHoverRadius: 10, //设置鼠标移动上去后圆点半径
+         tension: 0.1,
+         backgroundColor: [
+            'rgba(158, 221, 255, 0.5)',
+            // 'rgba(54, 162, 235, 0.2)',
+            // 'rgba(255, 206, 86, 0.2)',
+            // 'rgba(75, 192, 192, 0.2)',
+            // 'rgba(153, 102, 255, 0.2)',
+            // 'rgba(255, 159, 64, 0.2)'
+         ],
+         borderColor: [
             'rgba(100, 153, 233,1)',
-      ],
-    }]
-};
-const config = {
-  type: 'line', // 设置图表类型
-  data: data,
-  options: {
-      maintainAspectRatio: false,
-      plugins: {
-         legend: {
+         ],
+      }]
+   };
+   const config = {
+      type: 'line', // 设置图表类型
+      data: data,
+      options: {
+         maintainAspectRatio: false,
+         plugins: {
+            legend: {
+               display: false
+            },
+         },
+         title: {
             display: false
          },
-      },
-     title: {
-         display: false
-     },
-   }
-};
-const myChart = new Chart(ctx, config);
+      }
+   };
+   const myChart = new Chart(ctx, config);
+}
+
+
+
 
